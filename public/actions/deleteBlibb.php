0991737c5716ca39d6ba521a8e8b3ea550ed2387
Ivan Pedrazas <ipedrazas@gmail.com>
2012-07-25 15:17:11 +0100

    delete verb

--- public/actions/deleteBlibb.php
+++ public/actions/deleteBlibb.php
@@ -10,10 +10,7 @@ class DeleteBlibb extends lib {
         $user = require_login();
 		$bid = $this->gt("id");
 		$pest = new Pest(REST_API_URL);
-        $pestParams = array();
-        $pestParams['blibb_id'] = $bid;
-        $pestParams['login_key'] = getKey();        
-		$ret = $pest->post('/blibb/del',$pestParams);    	
+		$ret = $pest->delete('/blibb/'. $bid . '/'. getKey());    	
         $dest = '/user/'. $user;        
         header("Location: " . $dest);
     }
