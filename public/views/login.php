e04ddb4480dd3c87608d3cd75583972502d9a4c9
Ivan Pedrazas <ipedrazas@gmail.com>
2012-08-09 23:49:59 +0100

    login username label added

--- public/views/login.php
+++ public/views/login.php
@@ -5,8 +5,8 @@ require_once(__DIR__.'/../inc/header.php');
 <div class="container">
         <ul id="messages">
 
-        <?php 
-			if(isset($errorMsg)){        		
+        <?php
+			if(isset($errorMsg)){
 				echo $errorMsg;
 			}
         ?>
@@ -21,11 +21,11 @@ require_once(__DIR__.'/../inc/header.php');
 					<input name="t" type="hidden" value="<?php echo $reqHash ?>" />
 				</div>
 				<p class"offset2">
-				  <label for="u">Email <small>Don't have an account? <a href="signup.php" tabindex="999">Sign up</a></small></label>
+				  <label for="u">Username or Email <small>Don't have an account? <a href="signup.php" tabindex="999">Sign up</a></small></label>
 
 				  <input id="u" class="span4" name="u" placeholder="email@example.com" size="30" type="text" />
 				</p>
-				
+
 
 				<p class"offset2">
 				  <label for="p">Password <small><a href="forgotpassword.php" tabindex="999">Forgot your password?</a></small></label>
