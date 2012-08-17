e341dd457bab03c3c7d5a02116f93b86caf6c0cd
Ivan Pedrazas <ipedrazas@gmail.com>
2012-08-17 10:09:31 +0100

    multi added

--- public/js/actions.js
+++ public/js/actions.js
@@ -15,7 +15,7 @@
 
 		$(this).append($('#'+id).html());
 
-		
+
 
 	});
 
@@ -29,8 +29,8 @@
 		$("form").delegate(".component", "mousedown", function(md){
 			md.preventDefault();
 		});
-			
-		
+
+
 
 	});
 
@@ -41,12 +41,12 @@
 		var parent = $(this).parent().parent().parent();
 		var input = parent.find('input');
 		var value = input.val();
-		
+
 		if (value !== '') {
 			input.before('<div class="item-list"><span class="item">'+value+'</span><span class="help-inline"><a href="#"><i class="icon-minus"></i></a></span></div>');
 			input.val('');
 		}
-		
+
 	});
 
 	$('.help-inline a .icon-minus').on('click', function(event, ui) {
@@ -58,13 +58,14 @@
 		var control = [];
 		var array = $('#form-builder .control-group');
 		var template_id = $(this).attr('data-tid');
-		
+
 		for (var i = 0, max = array.length; i < max; i++) {
 			c = {};
 			c.order = i + 1;
 			if ($(array[i]).attr('data-cid') === 'control-list') {
 				c.name = $(array[i]).find('p.control-label').html();
 				c.help = $(array[i]).find('.controls').find('p.help-block').html();
+				c.multi = $(array[i]).find('.controls').find('p.multi-block').find('input').is(':checked');
 				c.cid = $(array[i]).attr('id');
 				c.type = $(array[i]).find('.controls').find('input').attr('id').substring(0, 2);
 				c.items = [];
@@ -77,9 +78,9 @@
 				c.help = $(array[i]).children()[1].children[1].innerHTML;
 				c.cid = $(array[i]).attr('id');
 				c.type = $(array[i]).children()[1].children[0].id.substring(0, 2);
-
+				c.multi = $(array[i]).find('.controls').find('p.multi-block').find('input').is(':checked');
 			}
-			
+
 			control.push(c);
 		}
 		console.log(control);
@@ -96,39 +97,8 @@
 				$('#publishForm').show();
 			}
 		});
-		
+
 	});
-
-	$( '#generateForm2' ).click(function() {
-			var control = [];
-			var array = $('#form-builder .control-group');
-			for (var i = 0, max = array.length; i < max; i++) {
-				c = {};
-				c['order'] = i + 1;
-				c['name'] = $(array[i]).children()[0].innerHTML;
-				c['help'] = $(array[i]).children()[1].children[1].innerHTML;
-				c['cid'] = $(array[i]).attr('id');
-				c['type'] = $(array[i]).children()[1].children[0].id.substring(0, 2);
-				control.push(c);
-			}
-			$.ajax({
-				url: 'actions/setControlsData',
-				type: "POST",
-				data: {
-					control: control,
-					template: '<?php echo $tid; ?>'
-				},
-				success: function(msg) {
-					$alert = "<div class='alert alert-success'><a class='close' data-dismiss='alert'>×</a>Template generated succesfully!<br> You can publish it to make it available or leave i as Draft.</div>";
-					$('#buttonRack').before($alert);
-					$('#publishForm').show();
-				}
-			});
-		});
-
-		$( '#publishForm' ).click(function() {
-			$('#dynForm').submit();
-		});
 
 
 })(jQuery);
