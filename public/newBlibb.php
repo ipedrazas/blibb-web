06634fa1e33c9b8f593162e3e59594ec8d90fcef
Ivan Pedrazas <ipedrazas@gmail.com>
2012-09-11 21:21:25 +0100

    template refactor

--- public/newBlibb.php
+++ public/newBlibb.php
@@ -14,24 +14,17 @@ class Application extends lib {
     	$pest = new Pest(REST_API_URL);
     	$jts = $pest->get('/template/active/n,d,t');
  		$rs = json_decode($jts);
-
  		$count = $rs->count;
 		$m = new Mustache();
  		$templates = array();
  		$template_html = '<dl id="{{id}}"><dt><img src="/img/templates/draft.png" hspace="10" width="50" height="60"></dt><dd>{{name}}</dd></dl>';
  		$t = '';
- 		if($count>0){
- 			$resultset = $rs->resultset;
- 			foreach ($resultset as $jt) {
- 				$template = json_decode($jt);
- 				$a = '$oid';
- 				$at['id'] = $template->_id->$a;
- 				$at['name'] = $template->n;
- 				$t .= $m->render($template_html, $at);
+		foreach ($rs as $template) {
+			$at['id'] = $template->id;
+			$at['name'] = $template->name;
+			$t .= $m->render($template_html, $at);
 
- 			}
- 		}
-
+		}
  		$this->render('newBlibb',compact('current_user','t'));
 
     }
