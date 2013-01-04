f80e868e53348e6b8851f227281145076755ec69
Ivan Pedrazas <ipedrazas@gmail.com>
2013-01-04 19:32:09 +0000

    align with api

--- public/getNotified.php
+++ public/getNotified.php
@@ -8,29 +8,29 @@ class Application extends lib {
     	$mail = new BMail();
     	$common = new Common();
 		$email = $this->gt("email");
-    	
+
    	// Add email to DB
-   		$ip = $common->getIpAddress();	
+   		$ip = $common->getIpAddress();
 		$browser = $_SERVER['HTTP_USER_AGENT'];
-		
-		
+
+
 
 	 	$subject = 'Blibb Notification List';
 		$file = __DIR__."/../data/mail.html";
-		$contents = file($file); 
+		$contents = file($file);
 		$html = implode($contents);
 
 		$file2 = __DIR__."/../data/mail.txt";
-		$contents2 = file($file2); 
+		$contents2 = file($file2);
 		$text = implode($contents2);
 
 		$from = 'info@blibb.net';
 		$fromName = 'Blibb';
 		$mail->sendMail($email, $email, $subject, $html, $text);
-	
+
 		$msg = "<h2>Thanks!</h2>Stay tunned, we're planning to start sending invitations pretty soon!";
 		$pest = new Pest(REST_API_URL);
-		$result = $pest->post('/sys/addToBeta',array(
+		$result = $pest->post('/sys/add/tobeta',array(
 			'email' => $email,
 			'ip' => $ip,
 			'browser' => $browser
@@ -42,4 +42,4 @@ class Application extends lib {
 }
 
 $app = new Application();
-$app->run();  
+$app->run();
