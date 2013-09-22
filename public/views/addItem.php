7db4eaef6183be4cdf684cf8d963d1f2900ce5c2
Ivan Pedrazas <ipedrazas@gmail.com>
2013-09-22 23:21:46 +0100

    Upload Images control handler added

--- public/views/addItem.php
+++ public/views/addItem.php
@@ -5,24 +5,24 @@ require_once(__DIR__.'/../inc/header.php');
 <link type="text/css" href="/css/addItem.css" rel="stylesheet" />
 <link rel="stylesheet" href="/css/user.css">
 <link href="/css/fileuploader.css" rel="stylesheet" type="text/css">
-	
-<link type="text/css" href="/css/blitzer/jquery-ui-1.8.16.custom.css" rel="stylesheet" />	
-<script type="text/javascript" src="/js/libs/jquery.ui.core.js"></script> 
-<script type="text/javascript" src="/js/libs/jquery.ui.widget.js"></script> 
-<script type="text/javascript" src="/js/libs/jquery.ui.datepicker.js"></script> 
+
+<link type="text/css" href="/css/blitzer/jquery-ui-1.8.16.custom.css" rel="stylesheet" />
+<script type="text/javascript" src="/js/libs/jquery.ui.core.js"></script>
+<script type="text/javascript" src="/js/libs/jquery.ui.widget.js"></script>
+<script type="text/javascript" src="/js/libs/jquery.ui.datepicker.js"></script>
 <script>
 	$(function() {
 	    	$( "#datepicker" ).datepicker();
 		});
-	$('a[name=addItem]').live("click", function(){	
+	$('a[name=addItem]').live("click", function(){
 		$('#addItem').submit();
-	}); 
-	
-	$('a[name=cancel]').live("click", function(){	
+	});
+
+	$('a[name=cancel]').live("click", function(){
 		$('#addItem')[ 0 ].reset();
 	});
 
-	
+
 </script>
 
 
@@ -48,12 +48,48 @@ require_once(__DIR__.'/../inc/header.php');
 					<li><a name="cancel" href="#" class="btn">Cancel</a></li>
 					<li><a name="addItem" href="#" class="btn btn-primary">Add Item</a></li>
 				</ul>
-			</fieldset>		
+			</fieldset>
 		</form>
+		<div id="upload_image_file" style="display:none;">
+		    <form enctype="multipart/form-data" id="upload_image_file_blibb">
+		        <input name="file" type="file" />
+		        <input type="button" id="image_btn" value="Upload" />
+
+		    	<progress style="display:none"></progress>
+		    <div id="result"></div>
+		</div>
 	</div>
 
 
 
+<script type="text/javascript">
+
+$('#image_btn').click(function(){
+    $('progress').show();
+    var formData = new FormData($('#upload_image_file_blibb')[0]);
+    formData.append('login_key', '<?php echo getKey() ?>');
+    $.ajax({
+        url: '<?php echo REST_API_URL ?>/image/upload',
+        type: 'POST',
+        xhr: function() {
+            myXhr = $.ajaxSettings.xhr();
+            return myXhr;
+        },
+        success: completeHandler,
+        error: errorHandler,
+        data: formData,
+        cache: false,
+        contentType: false,
+        processData: false
+    });
+});
+
+function errorHandler(e){
+    $('progress').hide();
+    var alert = "<div class='alert alert-error'><a class='close' data-dismiss='alert'>×</a>There was an error uploading your picture!</div>";
+    $('#imagebox').after(alert);
+}
+</script>
 <?php
 require_once(__DIR__.'/../inc/footer.php');
 ?>
